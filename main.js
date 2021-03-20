c397f60edea7e7ece621a132cf08d05f8cf66a8d
Vinicius_Lopes_Alves <vinicius.lopes@foster.com.br>
2021-03-19 22:36:56 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-const load = () => {
+
+function load (){
     document.getElementById("loading").style.display = "none";
     document.getElementById("conteudo").style.display = "inherit";
 }
 
 window.onload = (function () {
     window.setInterval('load()',2000);})
+
+function browserVerify() {
+
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
+           //alert('Opera');
+    }
+    else if (navigator.userAgent.indexOf("Chrome") != -1) {
+        //alert('Chrome');
+        //gconsole.log(window.navigator.userAgent)
+    }
+    else if (navigator.userAgent.indexOf("Firefox") != -1) {
+        //alert('Firefox');
+    }
+    else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
+    {
+        //alert('Explorer');
+        //console.log(window.navigator.userAgent)
+    }
+    else if (navigator.userAgent.indexOf("Safari") != -1) {
+        document.getElementById("main-site").style.display = 'none';
+        document.getElementById("not-suported").style.display = 'block';
+    }
+    else {
+          alert('unknown');
+    }
+}
