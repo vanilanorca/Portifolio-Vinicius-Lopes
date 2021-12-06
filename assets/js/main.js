1db3c5446fc52966348fb9191cb2525bae70977d
vanilanorca <vilopesalves@hotmail.com>
2021-12-05 23:50:39 -0300

    carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@ function load (){
     document.getElementById("conteudo").style.display = "inherit";
 }
 
-window.onload = (function () {
-    window.setInterval('load()',2000);})
+window.onload = load()
+// window.onload = (function () { window.setInterval('load()',2000);})
 
 
 
@@ -108,18 +108,34 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-// let swiper = new Swiper('.swiper-container', {
-//     cssMode: true,
-//     loop: false,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
+var slideIndex = 1;
+            showSlides(slideIndex);
+
+            // proximo/anterior controlers
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
+
+            // Thumb image controls
+            function currentSlide(n) {
+                showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+                var i;
+                var slides = document.getElementsByClassName("mySlides");
+                var dots = document.getElementsByClassName("dot");
+                if (n > slides.length) { slideIndex = 1 }
+                if (n < 1) { slideIndex = slides.length }
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex - 1].style.display = "block";
+                dots[slideIndex - 1].className += " active";
+            }
 
 
 
@@ -130,60 +146,6 @@ modalCloses.forEach((modalClose) =>{
 
 
 
-
-$.Slicebox.defaults = {
-	// (v)ertical, (h)orizontal or (r)andom
-	orientation : 'v',
-
-	// perspective value
-	perspective : 1200,
-
-	// number of slices / cuboids
-	// needs to be an odd number 15 => number > 0 (if you want the limit higher, 
-	// change the _validate function).
-	cuboidsCount : 5,
-
-	// if true then the number of slices / cuboids is going to be random (cuboidsCount is overwitten)
-	cuboidsRandom : false,
-
-	// the range of possible number of cuboids if cuboidsRandom is true
-	// it is strongly recommended that you do not set a very large number :)
-	maxCuboidsCount : 5,
-
-	// each cuboid will move x pixels left / top (depending on orientation). 
-	// The middle cuboid doesn't move. the middle cuboid's neighbors will 
-	// move disperseFactor pixels
-	disperseFactor : 0,
-
-	// color of the hidden sides
-	colorHiddenSides : '#222',
-
-	// the animation will start from left to right. The left most 
-	// cuboid will be the first one to rotate
-	// this is the interval between each rotation in ms
-	sequentialFactor : 150,
-
-	// animation speed
-	// this is the speed that takes "1" cuboid to rotate
-	speed : 600,
-
-	// transition easing
-	easing : 'ease',
-
-	// if true the slicebox will start the animation automatically
-	autoplay : false,
-
-	// time (ms) between each rotation, if autoplay is true
-	interval: 3000,
-
-	// the fallback will just fade out / fade in the items
-	// this is the time fr the fade effect
-	fallbackFadeSpeed : 300,
-	
-	// callbacks
-	onBeforeChange : function( position ) { return false; },
-	onAfterChange : function( position ) { return false; }
-};
 
 
 
