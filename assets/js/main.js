c5b9558ab8cf559f8877fecb7182509aa0fdbc53
Vinicius Lopes Alves <vilopesalves@hotmail.com>
2022-11-23 17:44:38 -0300

    portifolio de motion design

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,6 +136,36 @@ var slideIndex = 1;
                 dots[slideIndex - 1].className += " active";
             }
 
+// ---------------------------------
+
+var slideIndex2 = 1;
+            showSlides2(slideIndex2);
+
+            // proximo/anterior controlers
+            function plusSlides2(n) {
+                showSlides2(slideIndex2 += n);
+            }
+
+            // Thumb image controls
+            function currentSlide(n) {
+                showSlides2(slideIndex2 = n);
+            }
+
+            function showSlides2(n) {
+                var i;
+                var slides2 = document.getElementsByClassName("mySlides2");
+                var dots2 = document.getElementsByClassName("dot2");
+                if (n > slides2.length) { slideIndex2 = 1 }
+                if (n < 1) { slideIndex2 = slides2.length }
+                for (i = 0; i < slides2.length; i++) {
+                    slides2[i].style.display = "none";
+                }
+                for (i = 0; i < dots2.length; i++) {
+                    dots2[i].className = dots2[i].className.replace(" active", "");
+                }
+                slides2[slideIndex2 - 1].style.display = "block";
+                dots2[slideIndex2 - 1].className += " active";
+            }
 
 
 
